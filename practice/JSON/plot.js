ebbb089361e0882a52988bdbf1cf4faaa6b393a2
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-12 20:39:52 -0400

    Adding practice work.

--- practice/JSON/plot.js
+++ practice/JSON/plot.js
@@ -2,3 +2,9 @@ d3.json("samples.json").then(function(data){
     console.log("hello!  Welcome to my json data.");
     console.log(data);
 });
+
+d3.json("samples.json").then(function(data){
+    let firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) => {console.log(key + ': ' + value);});
+
+});
